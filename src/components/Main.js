0217f2d078ffed0d9eee87be764eb0033a9ce3e3
Oscar Li <oscarli@Oscars-MBP-3.hsd1.ca.comcast.net>
2019-03-26 14:34:13 -0700

    initialized google map api using my API key

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,10 @@
-import React from 'react'
-
+import React from 'react';
+import Map from './Map'
 export class Main extends React.Component{e
     render() {
         return(
             <div className="main">
+                <Map />
             </div>
         );
     }
