7061f6032370115b5851aac4a039630c0c34bef0
Oscar Li <oscarli@Oscars-MBP-3.hsd1.ca.comcast.net>
2019-03-26 14:19:10 -0700

    less npm stuff

---
+++ src/components/Main.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export class Main extends React.Component{e
+    render() {
+        return(
+            <div className="main">
+            </div>
+        );
+    }
+}
