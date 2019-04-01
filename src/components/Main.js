e8cafc0af0fac695c512f9b9930c420ad4b8dbc5
Oscar Li <oscarli@Oscars-MBP-3.hsd1.ca.comcast.net>
2019-04-01 16:20:31 -0700

    added venue list on the right

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Map from './Map';
+import {RecommendedVenues} from './RecommendedVenues';
 
 export class Main extends React.Component {
     render() {
         return (
             <div className="main">
                 <Map />
+                <RecommendedVenues />
             </div>
         );
     }
